cd44a848bb51eddac19a654bf62e105325392b16
hari <talari.harikumar7@gmail.com>
2018-05-27 22:21:42 +0530

    validations for creating a teacher

--- app/models/teacher.rb
+++ app/models/teacher.rb
@@ -1,2 +1,5 @@
 class Teacher < ApplicationRecord
+	validates :name, presence: true
+	validates :age, presence: true
+	validates :experience, presence: true
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :teachers
-  root to:"home#index"
+  root "teachers#index"
 end
