569ef17135dbde7e54f0ae6c58f3d9b5b01c21cd
hari <talari.harikumar7@gmail.com>
2018-05-27 21:45:59 +0530

    devise gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+	
+  devise_for :users
   resources :students
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :teachers
-  root'teachers#index'
+  root to:"home#index"
 end
